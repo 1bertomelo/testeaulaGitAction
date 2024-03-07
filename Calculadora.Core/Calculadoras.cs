346b7338840d0e5fd7365cfd5ba66b062d8bd71f
Humberto Melo <humbertomelo@campus1.uniaraxa.edu.br>
2024-03-06 21:01:20 -0300

    versao 1 aula git action

---
+++ Calculadora.Core/Calculadoras.cs
@@ -0,0 +1,16 @@
+﻿namespace Calculadora.Core
+{
+	public static class Calculadoras
+	{
+		public  static int Somar(int n1, int n2) => n1 + n2 ;
+		public static int Subtrair(int n1, int n2) => n1 - n2;
+		public static int Multiplicar(int n1, int n2) => n1* n2;
+ 
+		public static int Divisao(int n1, int n2)
+		{
+			if (n2 == 0) throw new 
+					Exception("Não é possível dividir por Zero");
+			return n1 / n2;
+		}
+	}
+}
